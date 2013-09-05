5130d8d78630fea47e8aef1145ff3f0eca2da4c7
Jorge Martinez <jorge.manu87@gmail.com>
2013-09-05 02:30:48 -0300

    Primer inicializacion de file uploader

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,28 +1,55 @@
-// Cargar mapa en evento load
 $(document).ready(function(){
 
-	$("#map").gmap3({
-		// Geolocalizar dispositivo
-		getgeoloc: {
-			callback : function(latLng){
-				// Inicializar y centrar
-				if (latLng){
-					$(this).gmap3({
-						map:{
-							options:{
-								center: latLng,
-								zoom: 16,
-								mapTypeId: google.maps.MapTypeId.TERRAIN
-							}
+$("#map").gmap3({
+	// Geolocalizar dispositivo
+	getgeoloc: {
+		callback : function(latLng){
+			// Inicializar y centrar
+			if (latLng){
+				$(this).gmap3({
+					map:{
+						options:{
+							center: latLng,
+							zoom: 16,
+							mapTypeId: google.maps.MapTypeId.TERRAIN
 						}
-					});
-				}
-				// O preguntar lugar al usuario 
-				else {
-					// Disparar modal
-				}
+					}
+				});
+			}
+			// O preguntar lugar al usuario 
+			else {
+				// Disparar modal
 			}
 		}
-	})
-
+	}
 });
+
+
+/*jslint unparam: true */
+/*global window, $ */
+$(function () {
+    'use strict';
+    // Change this to the location of your server-side upload handler:
+    var url = window.location.hostname === 'blueimp.github.io' ?
+                '//jquery-file-upload.appspot.com/' : 'server/php/';
+    $('#fileupload').fileupload({
+        url: url,
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo('#files');
+            });
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .progress-bar').css(
+                'width',
+                progress + '%'
+            );
+        }
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
+});
+
+
+}); // ~ Omega
