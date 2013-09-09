a16681604a5f6f191f5882c8af9b2f78ee0a60d1
Jorge Martinez <jorge.manu87@gmail.com>
2013-09-09 01:54:41 -0300

    limpieza de JS

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
+// Mapa
+// -------------------------
+
+$("#map").gmap3({
+    // Geolocalizar dispositivo
+    getgeoloc: {
+        callback : function(latLng){
+            // Inicializar y centrar
+            if (latLng){
+                $(this).gmap3({
+                    map:{
+                        options:{
+                            center: latLng,
+                            zoom: 16,
+                            mapTypeId: google.maps.MapTypeId.TERRAIN
+                        }
+                    }
+                });
+            }
+            // O preguntar lugar al usuario 
+            else {
+                // Disparar modal
+            }
+        }
+    }
+});
+
+
 // Tabs del submisison form
 // -------------------------
 
@@ -9,71 +37,41 @@ $("#submission-form").easyWizard({
     submitButton: false
 });
 
-// Mapa
+
+// File input
 // -------------------------
 
-$("#map").gmap3({
-	// Geolocalizar dispositivo
-	getgeoloc: {
-		callback : function(latLng){
-			// Inicializar y centrar
-			if (latLng){
-				$(this).gmap3({
-					map:{
-						options:{
-							center: latLng,
-							zoom: 16,
-							mapTypeId: google.maps.MapTypeId.TERRAIN
-						}
-					}
-				});
-			}
-			// O preguntar lugar al usuario 
-			else {
-				// Disparar modal
-			}
-		}
-	}
-});
-
-
-/*jslint unparam: true */
-/*global window, $ */
-$(function () {
-    'use strict';
-    // Change this to the location of your server-side upload handler:
-    var url = window.location.hostname === 'blueimp.github.io' ?
-                '//jquery-file-upload.appspot.com/' : 'server/php/';
-    $('#fileupload-input').fileupload({
-        url: url,
-        dataType: 'json',
-        autoUpload: false,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-        maxFileSize: 5000000, // 5 MB
-        loadImageMaxFileSize: 15000000, // 15MB
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo('#files');
-            });
-        },
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            $('#progress .progress-bar').css(
-                'width',
-                progress + '%'
-            );
-        }
-    }).on('fileuploadprocessalways', function (e, data) {
-        var index = data.index,
-            file = data.files[index],
-            node = $("#preview");
-        if (file.preview) {
-            // node
-            //     .append('<br>')
-            //     .prepend(file.preview);
-            console.log(file);
-        }
-    });
+var uploadUrl = window.location.hostname === 'blueimp.github.io' ?
+            '//jquery-file-upload.appspot.com/' : 'server/php/';
+$('#fileupload-input').fileupload({
+    url: uploadUrl,
+    dataType: 'json',
+    autoUpload: false,
+    acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+    maxFileSize: 5000000, // 5 MB
+    loadImageMaxFileSize: 15000000, // 15MB
+    done: function (e, data) {
+        $.each(data.result.files, function (index, file) {
+            $('<p/>').text(file.name).appendTo('#files');
+        });
+    },
+    progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $('#progress .progress-bar').css(
+            'width',
+            progress + '%'
+        );
+    }
+}).on('fileuploadprocessalways', function (e, data) {
+    var index = data.index,
+        file = data.files[index],
+        node = $("#preview");
+    if (file.preview) {
+        // node
+        //     .append('<br>')
+        //     .prepend(file.preview);
+        console.log(file);
+    }
 });
 
 
