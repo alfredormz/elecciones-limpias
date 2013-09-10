7ce2d4687c96bf976031252fbb87c3ab0cc3dad4
Jorge Martinez <jorge.manu87@gmail.com>
2013-09-10 13:29:13 -0300

    Comente cosas en el js

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 // Mapa
 // -------------------------
 
+// Geolocalizar dispositivo y mostrar mapa al arrancar
 $("#map").gmap3({
-    // Geolocalizar dispositivo
     getgeoloc: {
         callback : function(latLng){
             // Inicializar y centrar
@@ -14,25 +14,27 @@ $("#map").gmap3({
                         options:{
                             center: latLng,
                             zoom: 16,
-                            mapTypeId: google.maps.MapTypeId.TERRAIN,
+                            mapTypeId: google.maps.MapTypeId.ROADMAP,
                             mapTypeControl: false,
                             navigationControl: false,
                             streetViewControl: false,
-                            overviewMapControl: false
+                            overviewMapControl: false,
+                            panControl: false
                         }
                     }
                 });
             }
-            // O preguntar lugar al usuario 
+            // O sino preguntar lugar al usuario 
             else {
-                // Disparar modal
+                // Mostrar modal con lugares recuperados de BD
+                // bootbox.alert("No pudimos geolocalizar tu dispositivo");
             }
         }
     }
 });
 
 
-// Tabs del submisison form
+// Scripts del submisison form
 // -------------------------
 
 $("#submission-form").easyWizard({
@@ -40,8 +42,10 @@ $("#submission-form").easyWizard({
     showButtons: false,
     submitButton: false,
     debug: true,
+    // after: Al entrar en un step
     after: function(wizardObj, prevStepObj, currentStepObj) {
-        // Step 2 - locate
+        // After Step 2 Locate
+        // ---------------
         if (currentStepObj.attr('id') == 'step-locate') {
             
             var currentLatLng,
@@ -58,6 +62,7 @@ $("#submission-form").easyWizard({
             $("#map").gmap3({
                 getgeoloc: {
                     callback : function(latLng){
+                        // Rellenar los campos de latlng con geolocalización o centro actual del mapa si no se puede geolocalizar
                         if (latLng) {
                             currentLatLng = latLng
                         }else{
@@ -67,13 +72,15 @@ $("#submission-form").easyWizard({
                         setSubmissionLatlng(currentLatLng);
 
                         $(this).gmap3({
+                            // Visalizar lugar
                             getmaxzoom:{ 
                                 latLng: currentLatLng,
                                 callback:function(zoom){
                                     map.setCenter( currentLatLng );
-                                    map.setZoom( zoom - 1 );
+                                    map.setZoom( zoom - 2 );
                                 }
                             },
+                            // Agregar un marcador arrastrable
                             marker:{ 
                                 latLng: currentLatLng,
                                 options: { 
@@ -93,48 +100,85 @@ $("#submission-form").easyWizard({
                     }
                 }
             });
+
+            // TODO: antes de agregar el marcador editable, habria que quitar todos los otros que ya estan en el mapa o cambiarlos de color para que el usuario entienda cual es el suyo
+
         } // Fin step 2 - locate
+    },
+
+    // before: Antes de salir de un step
+    before: function(wizardObj, currentStepObj, nextStepObj) {
+        // Before Step 2 Locate
+        // Antes de salir del paso 2 habría que tomar ese marcador y hacerlo no-arrastrable
+        // ---------------
+        if (currentStepObj.attr('id') == 'step-locate') {
+            // $("#map").gmap3({
+            //     get: {
+            //         name:"marker",
+            //         all: true,
+            //         callback: function(objs){
+            //             $.each(objs, function(i, obj){
+            //                 obj.setADraggable(false);
+            //             });
+            //         }
+            //     }
+            // });
+        }
     }
 });
 
 
 // File input
 // -------------------------
-$('#submission-file').fileupload({
+$('#submission-form').fileupload({
     dataType: 'json',
     url: '/upload',
     previewMaxWidth: 200,
     previewMaxHeight: 200,
     previewCrop: true,
-    add: function(e, data){
-        $.each(data.files, function (index, file) {
-            var loadingImage = loadImage(
-                file,
-                function (img) {
-                    $("#upload-preview").html(img);
-                },
-                { maxWidth: 150, maxHeight: 150, crop: true }
-            );
-            if(loadingImage){
-                $('#submission-form').easyWizard('nextStep');
-            }
-            // $('#upload').click(function () {
-            //     data.submit();
-            // });
-            $('#cancel').click(function () {
-                data.abort();
-                $(".step-upload").show();
-                $(".step-positioning").hide();
-                $("#upload-preview").html("");
-            });
-
+    autoupload: false
+}).on('fileuploadadd', function(e, data){
+    $.each(data.files, function (index, file) {
+        var loadingImage = loadImage(
+            file,
+            function (img) {
+                $("#upload-preview").html(img);
+            },
+            { maxWidth: 150, maxHeight: 150, crop: true }
+        );
+        if(loadingImage){
+            $('#submission-form').easyWizard('goToStep', 2);
+        }
+        // $('#upload').click(function () {
+        //     data.submit();
+        // });
+        $('#submission-cancel').click(function () {
+            data.abort();
+            $(".step-upload").show();
+            $(".step-positioning").hide();
+            $("#upload-preview").html("");
         });
-    },
-    done: function(e, data){
-        // Mostrar el mapa con la foto recién cargada
-        $("#upload-preview").html("");
-        $('#submission-form').easyWizard('nextStep');
-    }
-  });
+    });
+}).on('fileuploadprogressall', function (e, data) {
+    var progress = parseInt(data.loaded / data.total * 100, 10);
+    $('#upload-progress .progress-bar').css(
+        'width',
+        progress + '%'
+    );
+}).on('fileuploaddone', function (e, data) {
+    // TODO: Mostrar el mapa con la foto recién cargada desplegada y volver el wizard al paso 1
+    $("#upload-preview").html("");
+});
+
+
+
+$("#step-locate-done").click(function(){
+    $('#submission-form').easyWizard('goToStep', 3);
+});
+
+$("#submission-cancel").click(function(){
+    $('#submission-form').easyWizard('goToStep', 1);
+});
+
 
 }); // ~ Omega
